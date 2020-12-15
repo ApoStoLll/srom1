b090e8239653fe6dc5b6b627f103438169f8c0c3
neket <neketapostolov@gmail.com>
2020-12-15 22:30:34 +0200

    all ready krome tests

--- lab2.py
+++ lab2.py
@@ -48,21 +48,28 @@ def lcm(num1, num2): #bin
     return num3 #Number
 
 def barret(x, n, m):
-    q = killDigits(x.copy(), len(n) - 1)
+    if len(x) != 2 * len(n):
+        #print("ez")
+        return div(x,n)[1]
+    q = killDigits(x, len(n) - 1)
     q = mul(q, m, 2).numBig
-    q = killDigits(x.copy(), len(n) + 1)
-    r = sub(x.copy(), mul(q.copy(), n.copy(), 2), 2).numBig
-    return div(x,n)[1]
+    q = killDigits(x, len(n) + 1)
+    r = sub(x.copy(), mul(q.copy(), n.copy(), 2).numBig, 2).numBig
+    print("r : ", r, "n :  ", n)
+    while compare(r, n):
+        r = sub(r, n, 2).numBig
+    return r
+
 
 def killDigits(x, k):
-    if x != None:
+    if x == None:
         return x
     for i in range(0, k):
         del x[-i+1]
     return x
 
 def addMod(num1, num2, modBin, beta):
-    num4 = add(num1, num2, beta)
+    num4 = add(num1, num2, 2)
     num4Bin = Mapper.mapDecToBin(num4.numDec)
     m = 2 ** len(num4Bin)
     mBin = Mapper.mapDecToBin(m)
@@ -71,7 +78,7 @@ def addMod(num1, num2, modBin, beta):
     return num5 #Bin
 
 def subMod(num1, num2, modBin, beta):
-    num4 = sub(num1, num2, beta)
+    num4 = sub(num1, num2, 2)
     num4Bin = Mapper.mapDecToBin(num4.numDec)
     m = 2 ** len(num4Bin)
     mBin = Mapper.mapDecToBin(m)
@@ -90,7 +97,7 @@ def mulMod(num1, num2, modBin, beta):
 
 def gorner(a, b, n):
     c = [1]
-    m = 2 ** (len(n) * 2)
+    m = 2 ** (len(n))
     mBin = Mapper.mapDecToBin(m)
     m = div(mBin, n)[0]
     b.reverse()
@@ -101,19 +108,103 @@ def gorner(a, b, n):
     return c
 
 def mainLab2():
-    beta = 2 ** 64
-    number1 = Number(beta, "D4D2110984907B5625309D956521BAB4157B8B1ECE04043249A3D379AC112E5B9AF44E721E148D88A942744CF56A06B92D28A0DB950FE4CED2B41A0BD38BCE7D0BE1055CF5DE38F2A588C2C9A79A75011058C320A7B661C6CE1C36C7D870758307E5D2CF07D9B6E8D529779B6B2910DD17B6766A7EFEE215A98CAC300F2827DB")
-    number2 = Number(beta, "3A7EF2554E8940FA9B93B2A5E822CC7BB262F4A14159E4318CAE3ABF5AEB1022EC6D01DEFAB48B528868679D649B445A753684C13F6C3ADBAB059D635A2882090FC166EA9F0AAACD16A062149E4A0952F7FAAB14A0E9D3CB0BE9200DBD3B0342496421826919148E617AF1DB66978B1FCD28F8408506B79979CCBCC7F7E5FDE7")
-    mod = Number(beta, "87D6D58D3991D536544389CEFA72FD0EBED75B2EBDC2C79BC3717793108F0952011E7E2D7040FFFB32F10BEB8ED0A485026B6860020B230128A8222B0525A6888942FB01C537800BF25D6F021D4B99D3CBD6DF9055FA22F91A6CFC4FDFC408AEF78F6418D3CE4E20EC7888B61BAE3D73C27C257CCA905DE0353C3A7CFFD9FE15")
+    #checkAdd()
+    #checkGcd()
+    #checkLcm()
+    #checkSub()
+    #checkMul()
+    checkPow()
 
+
+
+def checkGcd():
+    beta = 2 ** 64
+    number1 = Number(beta, "1EAEDD395588036066915AF60F3F84502967BD8617DC")
+    number2 = Number(beta, "1253FBED85830A10694A33E1C0DF38E62C8F6B2575B1")
     num1Bin = Mapper.mapDecToBin(number2.numDec)
     num2Bin = Mapper.mapDecToBin(number1.numDec)
-    mainBinLab2(num1Bin, num2Bin)
-
-def mainBinLab2(number1, number2):
-    print("start gcd")
-    res = gcd(number1, number2)
+    res = gcd(num1Bin, num2Bin)
     print(res.hex)
+
+def checkLcm():
+    beta = 2 ** 64
+    number1 = Number(beta, "1EAEDD395588036066915AF60F3F84502967BD8617DC")
+    number2 = Number(beta, "1253FBED85830A10694A33E1C0DF38E62C8F6B2575B1")
+    num1Bin = Mapper.mapDecToBin(number2.numDec)
+    num2Bin = Mapper.mapDecToBin(number1.numDec)
+    res = lcm(num1Bin, num2Bin)
+    print(res.hex)
+
+def checkAdd():
+    beta = 1024
+    number1 = Number(beta, "1EAEDD395588036066915AF60F3F84502967BD8617DC")
+    number2 = Number(beta, "1253FBED85830A10694A33E1C0DF38E62C8F6B2575B1")
+    mod = Number(beta, "247A")
+    num1Bin = Mapper.mapDecToBin(number2.numDec)
+    num2Bin = Mapper.mapDecToBin(number1.numDec)
+    modBin = Mapper.mapDecToBin(mod.numDec)
+    res = addMod(num1Bin, num2Bin, modBin, 2)
+    #print(res)
+    resDec = Mapper.mapBinToDec(res)
+    resNum = Number(beta, resDec, "dec")
+    print(resNum.hex)
+
+def checkSub():
+    beta = 1024
+    number1 = Number(beta, "1EAEDD395588036066915AF60F3F84502967BD8617DC")
+    number2 = Number(beta, "1253FBED85830A10694A33E1C0DF38E62C8F6B2575B1")
+    mod = Number(beta, "247A")
+    num1Bin = Mapper.mapDecToBin(number2.numDec)
+    num2Bin = Mapper.mapDecToBin(number1.numDec)
+    modBin = Mapper.mapDecToBin(mod.numDec)
+    res = subMod(num2Bin, num1Bin, modBin, 2)
+    #print(res)
+    resDec = Mapper.mapBinToDec(res)
+    resNum = Number(beta, resDec, "dec")
+    print(resNum.hex)
+
+def checkMul():
+    beta = 1024
+    number1 = Number(beta, "1EAEDD395588036066915AF60F3F84502967BD8617DC")
+    number2 = Number(beta, "1253FBED85830A10694A33E1C0DF38E62C8F6B2575B1")
+    mod = Number(beta, "247A")
+    num1Bin = Mapper.mapDecToBin(number2.numDec)
+    num2Bin = Mapper.mapDecToBin(number1.numDec)
+    modBin = Mapper.mapDecToBin(mod.numDec)
+    res = mulMod(num2Bin, num1Bin, modBin, 2)
+    #print(res)
+    resDec = Mapper.mapBinToDec(res)
+    resNum = Number(beta, resDec, "dec")
+    print(resNum.hex)
+
+def checkPow():
+    beta = 1024
+    number1 = Number(beta, "1EAEDD395588036066915AF60F3F84502967BD8617DC")
+    number2 = Number(beta, "1253FBED85830A10694A33E1C0DF38E62C8F6B2575B1")
+    mod = Number(beta, "247A")
+    num1Bin = Mapper.mapDecToBin(number2.numDec)
+    num2Bin = Mapper.mapDecToBin(number1.numDec)
+    modBin = Mapper.mapDecToBin(mod.numDec)
+    res = gorner(num2Bin, num1Bin, modBin)
+    #print(res)
+    resDec = Mapper.mapBinToDec(res)
+    resNum = Number(beta, resDec, "dec")
+    print(resNum.hex)
+
+def test1():
+    beta = 1024
+    number1 = Number(beta, "1EAEDD395588036066915AF60F3F84502967BD8617DC")
+    number2 = Number(beta, "1253FBED85830A10694A33E1C0DF38E62C8F6B2575B1")
+    number3 = Number(beta, "1253FBEF85830A10694A33E1C0DF38E62C8F6B2575B1")
+    mod = Number(beta, "247A")
+
+    num1Bin = Mapper.mapDecToBin(number1.numDec)
+    num2Bin = Mapper.mapDecToBin(number2.numDec)
+    num3Bin = Mapper.mapDecToBin(number3.numDec)
+    modBin = Mapper.mapDecToBin(mod.numDec)
+
+
+
 
 if __name__ == "__main__":
     mainLab2()
